38fbea67dfc359786ca599873e247571d4395cbd
ZengJiang <mhisf@vip.qq.com>
2018-06-05 22:59:39 +0800

    first commnit

---
+++ application/common/controller/Api.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dizy
+ * Date: 2018/6/5
+ * Time: 22:26
+ */
+
+namespace app\common\controller;
+
+
+use think\Controller;
+
+class Api extends Controller
+{
+
+}
---
+++ application/common/model/User.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dizy
+ * Date: 2018/6/5
+ * Time: 22:26
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class User extends Model
+{
+
+}
