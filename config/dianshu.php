29c99b0752f666e91a5146f884002e107407683b
DizyZeng <mhisf@vip.qq.com>
2018-06-09 23:36:58 +0800

    updated

--- config/dianshu.php
+++ config/dianshu.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'book_types' => ['Horror', 'Romance'],
+    'book_types' => ['Horror', 'Romance', 'Mystery', 'Comedy', 'sci-fi'],
     'upload_image_path' => 'http://ds.trealent.com/upload/image/',
 ];
