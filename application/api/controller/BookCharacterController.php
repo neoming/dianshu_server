fdc7a324a235042f629f6f8bf7d9972a335e845f
neoming <10165101145@stu.ecnu.edu.cn>
2018-06-09 18:04:40 +0800

    fixed Character Controller bug

--- application/api/controller/BookCharacterController.php
+++ application/api/controller/BookCharacterController.php
@@ -11,8 +11,7 @@ namespace app\api\controller;
 
 use app\common\controller\Api;
 use app\common\model\Book;
-use app\common\model\BookCharacter;
-
+use app\common\model\User;
 use app\common\validate\BookCharacterValidate;
 
 class BookCharacterController extends Api
